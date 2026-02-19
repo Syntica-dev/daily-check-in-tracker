025cea971503d9d54643cdc0d92608417a5d2ec0
Syntica <synticahsalethmary@gmail.com>
2026-02-19 16:41:19 +0530

    Initial commit  - Daily Check-in Tracker

---
+++ Daily_Check-In_Tracker/main.py
@@ -0,0 +1,26 @@
+print("Good Morning! Let's do a quick daily check-in")
+print("Did you wake up on time today? (yes/no)")
+response = input()
+if response.lower() == "yes":
+    print("Great job! You're on track.")
+else:
+    print("Try to improve your sleep schedule next time.")
+print("Did you have a healthy breakfast? (yes/no)")
+response = input()
+if response.lower() == "yes":
+    print("Awesome! Let's get ready for the day.")
+else:
+    print("Don't skip breakfast anymore, anyways let's get started for the day")
+print("Did you complete your python video lesson today? (yes/no)")
+response = input ()
+if response.lower() == "yes":
+    print("Great! You are leveling up your coding skills!")
+else:
+    print("Make sure to keep going with the lessons and stay consistent.")
+print("Did you write a page in your journal today? (yes/no)")
+response = input()
+if response.lower() == "yes":
+    print("Great job! Keep doing your morning pages.")
+else:
+    print("It's okay, try to write a page in your journal every day.")
+print("No matter what, stay positive and lively! You got this!")
